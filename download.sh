6baa47ccb69bc7eea6372350923e4a906a77c611
周詳 <hchou@moda.gov.tw>
2024-09-12 13:13:52 +0800

    Init

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p public
+while read -r line; do
+  if [ $(curl https://cdpn.io/_/fullpage/$line -w "%{http_code}" -o /dev/null -s) -eq 200 ]; then
+    curl https://cdpn.io/_/fullpage/$line -o public/$line.html -H "Referer: https://codepen.io/" -s
+    echo "$line download successful"
+  else
+    echo "$line download failed"
+  fi
+done < "list.txt"
